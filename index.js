061c472bd46c55ddb87dcadbae4120d69ca65fe4
Perpetual Akinyi <you@example.com>
2024-10-04 00:24:57 +0300

    find method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const record =[];
+function superbowlWin(array) {
+        const win = array.find(obj => obj.result === "W");
+        if (win) {
+          return win.year;
+        }
+        return undefined;
+     }
+      
